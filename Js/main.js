35b808e269456c693e6f3caf5997f88d2980d238
Anandhbasha <anadhakm@gmail.com>
2026-05-26 10:33:19 +0530

    js

---
+++ Js/main.js
@@ -0,0 +1,34 @@
+// variable
+// hoisting
+// var let const
+// console.log(a)
+// var a = 10
+// type->var
+//a ->variable name
+// = -> assignment operator ->right side irukka value left side la irukka variable assign pannum
+// 10-> value
+
+// var b =20
+
+// a=b
+// a=50
+// console.log(a)
+// var a = 10
+// var a =60
+// // print -> sep  cout>>"Hello\n" end
+// console.log("The a value is:"+a);
+// sep =" "
+
+// let
+//  let x = 50
+//  x=60
+//  console.log(x);
+
+
+//const 
+
+const a = 10
+a=20
+console.log(a);
+
+ 
